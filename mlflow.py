58f133d060ccc070517357d19476f0b79db01783
Omar Ali <omar.ali0703@gmail.com>
2021-11-16 16:37:21 +0000

    Updates

--- mlflow.py
+++ mlflow.py
@@ -3,8 +3,24 @@ import sys
 args=sys.args
 run_type = "all"
 project_name="untitled"
-accepted_l1_commands = ["create", "run"]
-accepted_l2_commands = ["pre","preprocess","eval","evaluate","results", "generate_results"]
+accepted_l1_commands = [
+	"create",
+	"run"
+]
+accepted_l2_commands = [
+	"pre",
+	"preprocess",
+	"eval",
+	"evaluate",
+	"results",
+	"generate_results",
+	"blank",
+	"tagging_flow"
+
+]
+
+mlflow_example_projects = "metadata/"
+
 if args[0] not in accepted_l1_commands or args[1] not in accepted_l2_commands:
 	sys.exit(f"command {arg[0]} {arg[1]}  does not exist."
 
