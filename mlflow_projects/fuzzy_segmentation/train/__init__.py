82f07aa66196224580e763b970c0dbda004a4116
Omar Ali <omar.ali0703@gmail.com>
2021-11-19 13:51:01 +0000

    Updates and project additions

---
+++ mlflow_projects/fuzzy_segmentation/train/__init__.py
@@ -0,0 +1,9 @@
+INPUT_FLOW = ""
+OUTPUT_FLOW = "train"
+# The order the subblocks are to be run in.
+BLOCK_ORDER = ["block_matlab_fuzzy_trainer",]
+
+
+# Entry point for a block
+def start():
+    print('Train block is running.')
---
+++ mlflow_projects/fuzzy_segmentation/visualiser/__init__.py
@@ -0,0 +1,9 @@
+INPUT_FLOW = ""
+OUTPUT_FLOW = "train"
+# The order the subblocks are to be run in.
+BLOCK_ORDER = ["block_visualiser", ]
+
+
+# Entry point for a block
+def start():
+    print('Producing results visualisations...')
